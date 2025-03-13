1022310d85c9616a727be3ef47888612093eabc0
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-03-14 04:11:16 +0530

    Get all blogs functionality completed

--- backend/src/routes/blogpost.route.js
+++ backend/src/routes/blogpost.route.js
@@ -42,4 +42,34 @@ router.post("/create-blog", async (req, res) => {
   }
 });
 
+//fetch all blogs
+router.get("/get-blogs", async (req, res) => {
+  try {
+    const blogs = await prisma.post.findMany({
+      include: {
+        author: {
+          select: {
+            id: true,
+            fullName: true,
+            userName: true,
+            email: true,
+            profilePic: true,
+          },
+        },
+        _count: {
+          select: { likes: true }, // Returns the count of likes
+        },
+      },
+      orderBy: { createdAt: "desc" }, // Sort by most recent blogs first
+    });
+
+    if (!blogs || blogs.length === 0)
+      return res.status(404).json({ error: "No blogs at the moment" });
+
+    return res.status(200).json(blogs);
+  } catch (error) {
+    console.log("Error in get-blogs controller", error.message);
+    res.status(500).json({ error: error.message });
+  }
+});
 export { router as blogPostRoute };
